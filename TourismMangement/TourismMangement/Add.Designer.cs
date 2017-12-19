daf6998027a2ee3262901206287b0e943530dd60
Mai Nour Al-Deen <mai.mohamed339@gmail.com>
2017-12-19 21:15:57 +0200

    Edit Update-Add-Login-Register Forms

--- TourismMangement/TourismMangement/Add.Designer.cs
+++ TourismMangement/TourismMangement/Add.Designer.cs
@@ -28,11 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ButtonAddTourplace = new System.Windows.Forms.Button();
+            this.ButtonAddUser = new System.Windows.Forms.Button();
+            this.ButtonAddTrans = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonAddTourplace
+            // 
+            this.ButtonAddTourplace.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ButtonAddTourplace.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ButtonAddTourplace.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonAddTourplace.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonAddTourplace.ForeColor = System.Drawing.Color.White;
+            this.ButtonAddTourplace.Location = new System.Drawing.Point(114, 342);
+            this.ButtonAddTourplace.Name = "ButtonAddTourplace";
+            this.ButtonAddTourplace.Size = new System.Drawing.Size(358, 60);
+            this.ButtonAddTourplace.TabIndex = 15;
+            this.ButtonAddTourplace.Text = "Add TourPlace";
+            this.ButtonAddTourplace.UseVisualStyleBackColor = false;
+            // 
+            // ButtonAddUser
+            // 
+            this.ButtonAddUser.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ButtonAddUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ButtonAddUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonAddUser.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonAddUser.ForeColor = System.Drawing.Color.White;
+            this.ButtonAddUser.Location = new System.Drawing.Point(114, 135);
+            this.ButtonAddUser.Name = "ButtonAddUser";
+            this.ButtonAddUser.Size = new System.Drawing.Size(358, 60);
+            this.ButtonAddUser.TabIndex = 14;
+            this.ButtonAddUser.Text = "Add User";
+            this.ButtonAddUser.UseVisualStyleBackColor = false;
+            // 
+            // ButtonAddTrans
+            // 
+            this.ButtonAddTrans.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ButtonAddTrans.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ButtonAddTrans.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonAddTrans.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonAddTrans.ForeColor = System.Drawing.Color.White;
+            this.ButtonAddTrans.Location = new System.Drawing.Point(114, 240);
+            this.ButtonAddTrans.Name = "ButtonAddTrans";
+            this.ButtonAddTrans.Size = new System.Drawing.Size(358, 60);
+            this.ButtonAddTrans.TabIndex = 13;
+            this.ButtonAddTrans.Text = "Add Transportation";
+            this.ButtonAddTrans.UseVisualStyleBackColor = false;
+            // 
+            // Add
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.ButtonAddTourplace);
+            this.Controls.Add(this.ButtonAddUser);
+            this.Controls.Add(this.ButtonAddTrans);
+            this.Name = "Add";
             this.Text = "Add";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ButtonAddTourplace;
+        private System.Windows.Forms.Button ButtonAddUser;
+        private System.Windows.Forms.Button ButtonAddTrans;
     }
 }
