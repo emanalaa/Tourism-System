f29028e3d1b754c1d4bb06fc753f6f208412be51
Mai Nour Al-Deen <mai.mohamed339@gmail.com>
2017-12-27 07:27:16 +0200

    changes

--- TourismMangement/TourismMangement/EditProfile.Designer.cs
+++ TourismMangement/TourismMangement/EditProfile.Designer.cs
@@ -263,6 +263,7 @@
             this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "EditProfile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Profile";
             this.ResumeLayout(false);
             this.PerformLayout();
